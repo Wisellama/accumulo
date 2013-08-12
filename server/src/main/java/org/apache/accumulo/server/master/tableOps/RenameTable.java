11ca559bc5ecd03d6590f50cc4516a2653c27789
Sean Hickey <tallirishllama@gmail.com>
2013-08-12 15:06:22 -0400

    ACCUMULO-802 fixed up some places in the code where a namespace might be null

--- server/src/main/java/org/apache/accumulo/server/master/tableOps/RenameTable.java
+++ server/src/main/java/org/apache/accumulo/server/master/tableOps/RenameTable.java
@@ -68,27 +68,27 @@ public class RenameTable extends MasterRepo {
       tm.addNamespaceToTable(tableId, namespaceId);
     }
     
-    newTableName = Tables.extractTableName(newTableName);
-    oldTableName = Tables.extractTableName(oldTableName);
-    
     IZooReaderWriter zoo = ZooReaderWriter.getRetryingInstance();
     
     Utils.tableNameLock.lock();
     try {
       Utils.checkTableDoesNotExist(instance, newTableName, tableId, TableOperation.RENAME);
       
+      final String newName = Tables.extractTableName(newTableName);
+      final String oldName = Tables.extractTableName(oldTableName);
+      
       final String tap = ZooUtil.getRoot(instance) + Constants.ZTABLES + "/" + tableId + Constants.ZTABLE_NAME;
       
       zoo.mutate(tap, null, null, new Mutator() {
         public byte[] mutate(byte[] current) throws Exception {
           final String currentName = new String(current);
-          if (currentName.equals(newTableName))
+          if (currentName.equals(newName))
             return null; // assume in this case the operation is running again, so we are done
-          if (!currentName.equals(oldTableName)) {
+          if (!currentName.equals(oldName)) {
             throw new ThriftTableOperationException(null, oldTableName, TableOperation.RENAME, TableOperationExceptionType.NOTFOUND,
                 "Name changed while processing");
           }
-          return newTableName.getBytes();
+          return newName.getBytes();
         }
       });
       Tables.clearCache(instance);
